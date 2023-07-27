872e44941e0f3b9b4dc1aab2278528e52fa0d31d
NiklasEntwickelt <103101799+NiklasEntwickelt@users.noreply.github.com>
2023-07-27 10:03:23 +0200

    Update code.js

--- js/code.js
+++ js/code.js
@@ -61,10 +61,7 @@ const storageAcceptance = () => {
 //Once page content is loaded
 window.addEventListener("DOMContentLoaded",() => {
 
-    
-    if(!storageAcceptance()) {
-        return;
-    }
+
     
     //Create an Automatic filled out Banner
     document.querySelector("#title").innerHTML = `<span id="company" contenteditable>${company}</span> - <span id="userName" contenteditable>${userName}</span>, <span contenteditable id="date">${today}</span>`;
